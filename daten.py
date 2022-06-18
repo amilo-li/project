de224a1fa19c8ab81ab313c54ef1177bc745049b
amilo-li <82661523+amilo-li@users.noreply.github.com>
2022-06-18 16:17:44 +0200

    filter completed

--- daten.py
+++ daten.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 
 # Funktion zur Speicherung der Daten aus dem Formular im JSON-File
-def speichern(antragsteller, projektleiter, titel, datum):
+def speichern(antragsteller, projektleiter, businessunit, titel, prio, startdatum, meilenstein1, meilenstein2,
+                        meilenstein3, meilenstein4, enddatum, erstesQuartal, zweitesQuartal, drittesQuartal, viertesQuartal, informatik, buchhaltung, marketing, entwicklung):
     datei = "ausgabe_dict.json"
     try:
         with open(datei) as open_file:
@@ -11,15 +12,35 @@ def speichern(antragsteller, projektleiter, titel, datum):
     except FileNotFoundError:
         datei_content = {}
 
+    sum_totalf = float(erstesQuartal) + float(zweitesQuartal) + float(drittesQuartal) + float(viertesQuartal)
+    sum_totale = (float(informatik) * 55.50) + (float(buchhaltung) * 55.50) + (float(marketing) * 55.50) + (float(entwicklung) * 55.50)
+    sum_totalI = sum_totalf + sum_totale
+
     datei_content[str(datetime.now())] = {'Antragsteller': antragsteller,
                                           'Projektleiter': projektleiter,
+                                          'Businessunit': businessunit,
                                           'Titel': titel,
-                                          'Datum': datum,
+                                          'Priorität': prio,
+                                          'Total Fremdleistungen': sum_totalf,
+                                          'Total Eigenleistungen': sum_totale,
+                                          'Total Investitionssumme': sum_totalI,
+                                          'Startdatum': startdatum,
+                                          'Meilenstein 1': meilenstein1,
+                                          'Meilenstein 2': meilenstein2,
+                                          'Meilenstein 3': meilenstein3,
+                                          'Meilenstein 4': meilenstein4,
+                                          'Enddatum': enddatum,
+                                          '1.Quartal': erstesQuartal,
+                                          '2.Quartal': zweitesQuartal,
+                                          '3.Quartal': drittesQuartal,
+                                          '4.Quartal': viertesQuartal,
+                                          'Informatik Aufwand': informatik,
+                                          'Buchhaltung Aufwand': buchhaltung,
+                                          'Marketing Aufwand': marketing,
+                                          'Entwicklung Aufwand': entwicklung,
                                           }
-
     with open(datei, "w") as open_file:
         json.dump(datei_content, open_file, indent=4)
-
 
 # Daten im JSON File laden
 def antrag_laden():
--- main.py
+++ main.py
@@ -23,10 +23,27 @@ def formular():
         # Felder des Formulares definieren
         antragsteller = request.form["antragsteller"]
         projektleiter = request.form["projektleiter"]
+        businessunit = request.form["businessunit"]
         titel = request.form["titel"]
-        datum = request.form["datum"]
+        prio = request.form["prio"]
+        startdatum = request.form["startdatum"]
+        meilenstein1 = request.form["meilenstein1"]
+        meilenstein2 = request.form["meilenstein2"]
+        meilenstein3 = request.form["meilenstein3"]
+        meilenstein4 = request.form["meilenstein4"]
+        enddatum = request.form["enddatum"]
+        erstesQuartal = request.form["erstesQuartal"]
+        zweitesQuartal = request.form["zweitesQuartal"]
+        drittesQuartal = request.form["drittesQuartal"]
+        viertesQuartal = request.form["viertesQuartal"]
+        informatik = request.form["informatik"]
+        buchhaltung = request.form["buchhaltung"]
+        marketing = request.form["marketing"]
+        entwicklung = request.form["entwicklung"]
+
         # Daten der Felder speichern
-        daten.speichern(antragsteller, projektleiter, titel, datum)
+        daten.speichern(antragsteller, projektleiter, businessunit, titel, prio, startdatum, meilenstein1, meilenstein2,
+                        meilenstein3, meilenstein4, enddatum, erstesQuartal, zweitesQuartal, drittesQuartal, viertesQuartal, informatik, buchhaltung, marketing, entwicklung)
 
         antrag_gespeichert = "Dein Investitionsantrag wurde gespeichert. Falls gewünscht kannst ein weiterer " \
                              "Antrag erfassen werden. "
@@ -50,12 +67,16 @@ def overview():
     # if-Bedingung, falls gefiltert werden möchte
     if request.method == 'POST':
         filtered = True
-        Projektantrag = request.form['Projektantrag']
+        businessunit = request.form['businessunit']
 
-        for key, antrag in eingabe.items():  # in dieser for-Schleife wird gefiltert und die leere Liste gefüllt
+        if businessunit != "":
+            filter_value = businessunit
+            filter_key = "Businessunit"
+
+
+        for key, antrag in eingabe.items():  # in dieser for-Schleife wird gefiltert und die leere Liste zu füllen
             if antrag[filter_key] == filter_value:
                 filter_list.append(antrag)
-
 
     # hier werden die Informationen mitgegeben, die im template "uebersicht" aufgerufen werden können
     return render_template('overview.html', data=eingabe, allitems=filter_list, Filter=filtered)
