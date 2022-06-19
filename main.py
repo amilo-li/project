a8a7b2e4c1f99ea87cec6aed1e8f49a9fad3c807
amilo-li <82661523+amilo-li@users.noreply.github.com>
2022-06-19 16:54:12 +0200

    write comments

--- main.py
+++ main.py
@@ -2,23 +2,28 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import daten
+import plotly.express as px
+from plotly.offline import plot
+import pandas as pd
 
 app = Flask(__name__)
+
+"""
+Ich erkläre hiermit, dass dieses Projekt selbstständig entwickelt wurde und keine anderen als die angegebenen Quellen benutzt wurden.
+"""
 
 
 # Home / Startseite
 @app.route('/', methods=['GET', 'POST'])
 def start():
-    # wird der Button "Senden" gedrückt, erscheint die Formularseite, sonst normal die Startseite
-    if request.method == 'POST':
-        return render_template('formular.html')
     return render_template('index.html', user="Samira")  # hier wird der Begrüssungsname mitgegeben (Samira)
 
 
-# Angaben im Formular erfassen
+# User Input Form Felder
+# Input Felder werden definiert und Funktion "speichern" aus daten.py wird aufgerufen
 @app.route("/formular", methods=["POST", "GET"])
 def formular():
-    # Ausführen, wenn auf den Button senden geklickt wird
+    # Ausführen, wenn Button senden (post)
     if request.method == "POST":
         # Felder des Formulares definieren
         antragsteller = request.form["antragsteller"]
@@ -41,30 +46,32 @@ def formular():
         marketing = request.form["marketing"]
         entwicklung = request.form["entwicklung"]
 
-        # Daten der Felder speichern
+        # Input der Felder speichern
+        # "speichern" Funktion in daten.py
         daten.speichern(antragsteller, projektleiter, businessunit, titel, prio, startdatum, meilenstein1, meilenstein2,
-                        meilenstein3, meilenstein4, enddatum, erstesQuartal, zweitesQuartal, drittesQuartal, viertesQuartal, informatik, buchhaltung, marketing, entwicklung)
+                        meilenstein3, meilenstein4, enddatum, erstesQuartal, zweitesQuartal, drittesQuartal,
+                        viertesQuartal, informatik, buchhaltung, marketing, entwicklung)
 
-        antrag_gespeichert = "Dein Investitionsantrag wurde gespeichert. Falls gewünscht kannst ein weiterer " \
+        # Success Meldung, wenn daten gespeichert sind
+        antrag_gespeichert = "Dein Investitionsantrag wurde gespeichert. Falls gewünscht kann ein weiterer " \
                              "Antrag erfassen werden. "
 
         return render_template("formular.html", antrag=antrag_gespeichert)
 
-    # Die leeren Felder aus formular.html werden dargestellt. Mittels Speichern Button (POST) werden die Daten mit
-    # der Funktion daten.speichern (definiert in der Datei daten.py) im JSON File gespeichert
     return render_template("formular.html")
 
 
-# Übersicht der Investitionsprojekte -> Daten werden aus daten.py geladen
+# Übersicht der Investitionsprojekte
+# Daten werden mittels der Funktion antrag_laden in daten.py vom JSON File geladen
 @app.route("/overview", methods=["GET", "POST"])
 def overview():
-    eingabe = daten.antrag_laden()
+    importeddata = daten.antrag_laden()  # Funktion antrag_laden in daten.py aufrufen
     filter_list = []
     filter_value = ""
     filter_key = ""
     filtered = False
 
-    # if-Bedingung, falls gefiltert werden möchte
+    # Wenn auf den Button-Filter geklickt wird
     if request.method == 'POST':
         filtered = True
         businessunit = request.form['businessunit']
@@ -73,19 +80,61 @@ def overview():
             filter_value = businessunit
             filter_key = "Businessunit"
 
+        # Füge jeden Eintrag, bei dem der Key Businessunit und der entsprechende Wert vom Dropdown Feld übereinstimmt
+        # der Liste hinzu.
+        for key, value in importeddata.items():
+            if value[filter_key] == filter_value:
+                filter_list.append(value)
 
-        for key, antrag in eingabe.items():  # in dieser for-Schleife wird gefiltert und die leere Liste zu füllen
-            if antrag[filter_key] == filter_value:
-                filter_list.append(antrag)
-
-    # hier werden die Informationen mitgegeben, die im template "uebersicht" aufgerufen werden können
-    return render_template('overview.html', data=eingabe, allitems=filter_list, Filter=filtered)
+    return render_template('overview.html', data=importeddata, allitems=filter_list, Filter=filtered)
 
 
-# Analysis
+# Auswertung der Daten
 @app.route("/analysis", methods=['GET', 'POST'])
 def analysis():
-    return render_template('analysis.html')
+    importdata = daten.antrag_laden()  # Funktion antrag_laden in daten.py aufrufen
+
+    # Erste Auswertung Balkendiagramm Investitionssumme / Geschäftsfeld
+    dictdiagram = {}
+
+    # Für jeden key und value der importierten Daten
+    # Dictionary erstellen mit key = Wert Businessunit und value = Wert Investitionssumme
+    for key, values in importdata.items():
+        businessunit = values["Businessunit"]
+        dictdiagram[businessunit] = values["Total Investitionssumme"]
+
+    x = list(dictdiagram.keys())  # businessunit
+    y = list(dictdiagram.values())  # total investitionssumme
+
+    # Erstellung des Balkendiagramms und Bennenung der Achsen
+    fig = px.bar(x=x, y=y, labels={"x": "Geschäftsfeld",
+                                   "y": "Investitionssumme"},
+                 title="Übersicht der Investitionssummen pro Geschäftsfeld")
+    # output_type = div, da mit fig.show() die html Seite nicht mehr ersichtlich wäre
+    bar = plot(fig, output_type="div")
+    # Quelle: https://plotly.com/python/figure-labels/
+
+    # Zweite Auswertung Gant-Chart Projekt Start- / Enddatum
+    dictgant = {}
+    # Für jeden key und value der importierten Daten
+    # Dictionary erstellen mit key = Wert Titel und values Wert Startdatum, Wert Enddatum
+    for key, values in importdata.items():
+        titel = values["Titel"]
+        startdatum = values["Startdatum"]
+        enddatum = values["Enddatum"]
+        build_dict = {titel: {"Startdatum": startdatum, "Enddatum": enddatum}}
+        dictgant.update(build_dict)
+
+    df = pd.DataFrame(dictgant)
+    # Erstellung des Gantt-Charts und Bennenung der Achsen
+    fig = px.timeline(df, x_start=startdatum, x_end=enddatum, y=titel)
+    # Sortierung der Achse y Titel
+    fig.update_yaxes(autorange="reversed")
+    # output_type = div, da mit fig.show() die html Seite nicht mehr ersichtlich wäre
+    gant = plot(fig, output_type="div")
+    # Quelle: https://plotly.com/python/gantt/
+
+    return render_template('analysis.html', viz_bar=bar, data=dictdiagram, viz_gantt=gantt)
 
 
 if __name__ == "__main__":
